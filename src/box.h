21f0020fcefde946d692326257877e37a7319e42
Bugrahan Temur <bugrahan.temur@gmail.com>
2023-11-26 13:01:07 +0100

    Add box structure

---
+++ src/box.h
@@ -0,0 +1,33 @@
+#ifndef LOX_BOX
+#define LOX_BOX
+
+#include <memory>
+
+/**
+ * A wrapper around unique_ptr. Allows recursive data structures with value
+ * semantics.
+ */
+template <typename T>
+class Box {
+  std::unique_ptr<T> impl_;
+
+ public:
+  Box(T&& object) : impl_(std::make_unique<T>(std::move(object))) {}
+  Box(T const& object) : impl_(std::make_unique<T>(object)) {}
+
+  Box(Box const& other) : Box(*other.impl_) {}
+  Box& operator=(Box const& other) {
+    *impl_ = *other.impl_;
+    return *this;
+  }
+
+  ~Box() = default;
+
+  T& operator*() { return *impl_; }
+  T const& operator*() const { return *impl_; }
+
+  T* operator->() { return impl_.get(); }
+  T const* operator->() const { return impl_.get(); }
+};
+
+#endif

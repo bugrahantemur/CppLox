b857293078cef5d2727773d8c7aabb63042b3e1b
Bugrahan Temur <bugrahan.temur@gmail.com>
2024-01-18 20:49:04 +0100

    Simplify main lox function

--- src/lox.cpp
+++ src/lox.cpp
@@ -8,7 +8,6 @@
 #include "./parser/parser.hpp"
 #include "./resolver/error.hpp"
 #include "./resolver/resolver.hpp"
-#include "./types/expression.hpp"
 #include "./types/function.hpp"
 #include "./types/statement.hpp"
 #include "./types/token.hpp"
@@ -16,64 +15,28 @@
 #include "./utils/reader.hpp"
 
 namespace LOX {
-class Lox {
- public:
-  auto run(std::string const &file_path) -> void {
-    auto const file_contents = Reader::read_file(file_path);
-
-    do_run(file_contents);
-
-    if (had_error) {
-      std::exit(65);
-    }
-
-    if (had_runtime_error) {
-      std::exit(70);
-    }
+auto run(std::string const &file_path) -> void {
+  try {
+    std::string const contents = Reader::read_file(file_path);
+    std::vector<Token> const tokens = Lexer::scan_tokens(contents);
+    std::vector<Statement> const statements = Parser::parse(tokens);
+    std::unordered_map<Token, std::size_t> const resolution =
+        Resolver::resolve(statements);
+    Interpreter::interpret(statements, resolution);
+  } catch (std::exception const &e) {
+    std::cerr << "Unhandled exception: " << e.what() << '\n';
+    std::exit(-1);
+    return;
   }
-
- private:
-  auto do_run(std::string const &contents) -> void {
-    try {
-      std::vector<Token> const tokens = Lexer::scan_tokens(contents);
-      std::vector<Statement> const statements = Parser::parse(tokens);
-      std::unordered_map<Token, std::size_t> const resolution =
-          Resolver::resolve(statements);
-      Interpreter::interpret(statements, resolution);
-    } catch (Lexer::Error const &e) {
-      had_error = true;
-      e.report();
-      return;
-    } catch (Parser::Error const &e) {
-      had_error = true;
-      e.report();
-      return;
-    } catch (Resolver::Error const &e) {
-      had_error = true;
-      e.report();
-      return;
-    } catch (RuntimeError const &e) {
-      had_runtime_error = true;
-      e.report();
-      return;
-    } catch (std::exception const &e) {
-      std::cerr << "Unhandled exception: " << e.what() << '\n';
-      std::exit(-1);
-      return;
-    }
-  }
-
-  bool had_error{false};
-  bool had_runtime_error{false};
-};
-
+}
 }  // namespace LOX
+
 auto main(int argc, char *argv[]) -> int {
   if (argc != 2) {
     std::cout << "Wrong! Correct usage: cpplox [script]\n";
   } else {
-    LOX::Lox lox;
-    lox.run(argv[1]);
+    LOX::run(argv[1]);
   }
+
   return 0;
 }

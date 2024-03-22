e9e078ed8fc76120bf52fdd1ba2696e7975d38f8
Bugrahan Temur <bugrahan.temur@gmail.com>
2024-03-22 23:27:36 +0100

    Add parsing step to compiler

--- src/ByteCode/Compiler/Compiler.cpp
+++ src/ByteCode/Compiler/Compiler.cpp
@@ -2,28 +2,20 @@
 
 #include <iostream>
 
+#include "../../Common/Parser/Parser.hpp"
 #include "../../Common/Scanner/Scanner.hpp"
+#include "../../Common/Types/Syntax/Statement.hpp"
 #include "../../Common/Types/Tokens/Token.hpp"
 #include "../../Common/Types/Tokens/TokenTypes.hpp"
 
 namespace LOX::ByteCode::Compiler {
 
 using LOX::Common::Types::Token;
+using LOX::Common::Types::Syntax::Statements::Statement;
 
 auto compile(std::string const& source) -> void {
   std::vector<Token> tokens = Common::Scanner::scan(source);
-  int line = -1;
-  for (Token const& token : tokens) {
-    if (token.line != line) {
-      std::cout << "   " << token.line << ' ';
-      line = token.line;
-    } else {
-      printf("   | ");
-    }
-    std::cout << static_cast<int>(token.type) << " '" << token.lexeme << "'\n";
-
-    if (token.type == LOX::Common::Types::Tokens::TokenType::EOFF) break;
-  }
+  std::vector<Statement> statements = Common::Parser::parse(tokens);
 }
 
 }  // namespace LOX::ByteCode::Compiler

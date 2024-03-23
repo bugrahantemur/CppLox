a31d8ec0503c666afdbde4e32471cdbe1b450bcd
Bugrahan Temur <bugrahan.temur@gmail.com>
2024-03-23 23:22:16 +0100

    Realize two-pass compilation
    ... doesn't do anything yet

--- src/ByteCode/Compiler/Compiler.cpp
+++ src/ByteCode/Compiler/Compiler.cpp
@@ -1,21 +1,54 @@
 #include "./Compiler.hpp"
 
-#include <iostream>
-
 #include "../../Common/Parser/Parser.hpp"
 #include "../../Common/Scanner/Scanner.hpp"
+#include "../../Common/Types/Syntax/Expression.hpp"
 #include "../../Common/Types/Syntax/Statement.hpp"
 #include "../../Common/Types/Tokens/Token.hpp"
-#include "../../Common/Types/Tokens/TokenTypes.hpp"
+#include "../Chunk/Chunk.hpp"
+#include "../OpCode/OpCode.hpp"
 
 namespace LOX::ByteCode::Compiler {
 
 using LOX::Common::Types::Token;
 using LOX::Common::Types::Syntax::Statements::Statement;
+using namespace LOX::Common::Types::Syntax::Expressions;
+
+auto emit_byte(Chunk& chunk, Byte byte, std::size_t line) -> void {
+  chunk.add_instruction(byte, line);
+}
+
+auto emit_bytes(Chunk& chunk, Byte byte1, std::size_t line1, Byte byte2,
+                std::size_t line2) -> void {
+  chunk.add_instruction(byte1, line1);
+  chunk.add_instruction(byte2, line2);
+}
+
+auto expression(Chunk& chunk, Expression const& expr) -> void {}
+
+auto unary(Chunk& chunk, UnaryExpr const& expr) -> void {
+  // Compile the operand
+  expression(chunk, expr.right);
+
+  // Emit the operator instruction
+  switch (expr.op.type) {
+    case Common::Types::Tokens::TokenType::MINUS:
+      emit_byte(chunk, OpCode::OP_NEGATE, expr.op.line);
+      break;
+    default:
+      return;  // Unreachable
+  }
+}
 
 auto compile(std::string const& source) -> void {
   std::vector<Token> tokens = Common::Scanner::scan(source);
   std::vector<Statement> statements = Common::Parser::parse(tokens);
+
+  Chunk chunk;
+
+  for (auto const& stmt : statements) {
+    // Pass
+  }
 }
 
 }  // namespace LOX::ByteCode::Compiler

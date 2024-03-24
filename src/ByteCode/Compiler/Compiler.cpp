959d3cc5f33001bc4228bc1c291a9e87041e73de
Bugrahan Temur <bugrahan.temur@gmail.com>
2024-03-24 23:36:07 +0100

    Add expression visitor to the compiler

--- src/ByteCode/Compiler/Compiler.cpp
+++ src/ByteCode/Compiler/Compiler.cpp
@@ -9,6 +9,7 @@
 #include "../../Common/Types/Syntax/Expression.hpp"
 #include "../../Common/Types/Syntax/Statement.hpp"
 #include "../../Common/Types/Tokens/Token.hpp"
+#include "../../Common/Types/Tokens/TokenTypes.hpp"
 #include "../Chunk/Chunk.hpp"
 #include "../OpCode/OpCode.hpp"
 
@@ -16,6 +17,7 @@ namespace LOX::ByteCode::Compiler {
 
 using LOX::Common::Types::Token;
 using LOX::Common::Types::Syntax::Statements::Statement;
+using LOX::Common::Types::Tokens::TokenType;
 using namespace LOX::Common::Types::Syntax::Expressions;
 
 auto emit_byte(Chunk& chunk, Byte byte, std::size_t line) -> void {
@@ -44,21 +46,70 @@ auto emit_constant(Chunk& chunk, Value const& value) -> void {
   emit_bytes(chunk, OpCode::OP_CONSTANT, 0, make_constant(chunk, value), 0);
 }
 
-auto expression(Chunk& chunk, Expression const& expr) -> void {}
+struct ExpressionCompiler {
+  auto operator()(std::monostate) -> void {}
 
-auto unary(Chunk& chunk, UnaryExpr const& expr) -> void {
-  // Compile the operand
-  expression(chunk, expr.right);
+  auto operator()(Box<LiteralExpr> const& expr) -> void {}
 
-  // Emit the operator instruction
-  switch (expr.op.type) {
-    case Common::Types::Tokens::TokenType::MINUS:
-      emit_byte(chunk, OpCode::OP_NEGATE, expr.op.line);
-      break;
-    default:
-      return;  // Unreachable
+  auto operator()(Box<SuperExpr> const& expr) -> void {}
+
+  auto operator()(Box<ThisExpr> const& expr) -> void {}
+
+  auto operator()(Box<VariableExpr> const& expr) -> void {}
+
+  auto operator()(Box<AssignmentExpr> const& expr) -> void {}
+
+  auto operator()(Box<BinaryExpr> const& expr) -> void {
+    // Compile the operands
+    std::visit(*this, expr->left);
+    std::visit(*this, expr->right);
+
+    // Emit the operator instruction
+    Token const& op{expr->op};
+    TokenType const op_type{op.type};
+    std::size_t const op_line{op.line};
+
+    if (op_type == TokenType::PLUS) {
+      emit_byte(chunk, OpCode::OP_ADD, op_line);
+    } else if (op_type == TokenType::MINUS) {
+      emit_byte(chunk, OpCode::OP_SUBTRACT, op_line);
+    } else if (op_type == TokenType::STAR) {
+      emit_byte(chunk, OpCode::OP_MULTIPLY, op_line);
+    } else if (op_type == TokenType::SLASH) {
+      emit_byte(chunk, OpCode::OP_DIVIDE, op_line);
+    }
   }
-}
+
+  auto operator()(Box<CallExpr> const& expr) -> void {}
+
+  auto operator()(Box<GetExpr> const& expr) -> void {}
+
+  auto operator()(Box<GroupingExpr> const& expr) -> void {
+    std::visit(*this, expr->expression);
+  }
+
+  auto operator()(Box<LogicalExpr> const& expr) -> void {}
+
+  auto operator()(Box<SetExpr> const& expr) -> void {}
+
+  auto operator()(Box<UnaryExpr> const& expr) -> void {
+    // Compile the operand
+    std::visit(*this, expr->right);
+
+    // Emit the operator instruction
+    Token const& op{expr->op};
+    TokenType const op_type{op.type};
+
+    if (op_type == TokenType::MINUS) {
+      emit_byte(chunk, OpCode::OP_NEGATE, expr->op.line);
+    }
+
+    if (op_type == TokenType::BANG) {
+    }
+  }
+
+  Chunk& chunk;
+};
 
 auto number(Chunk& chunk, Value const& value) -> void {
   emit_constant(chunk, value);
